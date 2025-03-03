58485d1fb370beb266fabd888dcf11cfae8931f6
Marc Giestin Louis Cordova <154757927+kazuha001@users.noreply.github.com>
2025-03-04 06:27:58 +0800

    Update Start.js

--- src/scenes/Start.js
+++ src/scenes/Start.js
@@ -84,26 +84,32 @@ export class Start extends Phaser.Scene {
             if (playersLogs[0].lifePoints >= 20) {
                 alert("Congratulation too " + playersLogs[0].name)
                 this.game.destroy(true)
+                window.location.reload()
             }
             if (playersLogs[1].lifePoints >= 20) {
                 alert("Congratulation too " + playersLogs[1].name)
                 this.game.destroy(true)
+                window.location.reload()
             }
             if (playersLogs[2].lifePoints >= 20) {
                 alert("Congratulation too " + playersLogs[2].name)
                 this.game.destroy(true)
+                window.location.reload()
             }
             if (playersLogs[3].lifePoints >= 20) {
                 alert("Congratulation too " + playersLogs[3].name)
                 this.game.destroy(true)
+                window.location.reload()
             }
             if (playersLogs[4].lifePoints >= 20) {
                 alert("Congratulation too " + playersLogs[4].name)
                 this.game.destroy(true)
+                window.location.reload()
             }
             if (playersLogs[5].lifePoints >= 20) {
                 alert("Congratulation too " + playersLogs[5].name)
                 this.game.destroy(true)
+                window.location.reload()
             }
 
             
@@ -111,6 +117,7 @@ export class Start extends Phaser.Scene {
 
         var totalLuck = playersLogs.reduce((sum, player) => sum + player.luck, 0);
 
+        setTimeout(() => {
             setInterval(() => {
             
             let boxResult = [RandomColors(), RandomColors(), RandomColors()]
@@ -269,6 +276,8 @@ export class Start extends Phaser.Scene {
              
 
             }, 3000) // Set Every 3 Second
+        }, 5000)
+            
 
         
         
@@ -549,5 +558,3 @@ export class Start extends Phaser.Scene {
 
 
 }
-
-
