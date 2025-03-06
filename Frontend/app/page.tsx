6a66088b0241977a774a9c0352b66c5f10762dad
kur0gir1 <lerib936@gmail.com>
2025-03-06 23:38:43 +0800

    update socket connection port, add WalletProvider, and include @types/ws plus more characters!

--- Frontend/app/page.tsx
+++ Frontend/app/page.tsx
@@ -1,13 +1,13 @@
 "use client"; // ✅ Required for client components
 
-// import { useState } from "react";
-// import ConnectWallet from "@/components/WalletButton";
-// import CheckBalance from "@/components/CheckBalance"; // Ensure this is a valid component
+import { useState } from "react";
+import ConnectWallet from "@/components/WalletButton";
+import CheckBalance from "@/components/CheckBalance"; // Ensure this is a valid component
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
 
 export default function Home() {
-  // const [walletConnect, setWalletConnected] = useState(false);
+  const [walletConnect, setWalletConnected] = useState(false);
   const router = useRouter();
 
   useEffect(() => {
@@ -16,8 +16,8 @@ export default function Home() {
 
   return (
     <main className="w-screen h-screen flex flex-wrap">
-      {/* <ConnectWallet setWalletConnected={setWalletConnected} />
-      {walletConnect && <CheckBalance />} */}
+      <ConnectWallet setWalletConnected={setWalletConnected} />
+      {walletConnect && <CheckBalance />}
     </main>
   );
 }
