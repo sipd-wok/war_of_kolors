eea39c8773005675e4395df0810edb1230b29ece
Marc Giestin Louis Cordova <154757927+kazuha001@users.noreply.github.com>
2025-03-12 14:25:55 +0800

    Update page.js

--- Frontend/app/MainGame/GameDemoV1/src/app/page.js
+++ Frontend/app/MainGame/GameDemoV1/src/app/page.js
@@ -1,6 +1,6 @@
 "use client"
 
-import dynamic from "next/dynamic";
+import { dynamic }  from "next/dynamic";
 
 // Dynamically import the PhaserComponent with SSR disabled
 const PhaserComponent = dynamic(() => import("../components/phaser"), {
