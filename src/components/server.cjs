7f33076961a7921bba31bdbc6836a9b3d6d64a05
Marc Giestin Louis Cordova <cordy@DESKTOP-4GJDLHC.>
2025-03-12 13:50:13 +0800

    elite Changes

--- src/components/server.cjs
+++ src/components/server.cjs
@@ -7,7 +7,7 @@ const server = http.createServer();
 // Start Socket.IO
 const io = new Server(server, {
     cors: {
-        origin: "http://100.76.40.4:3000", // Allow all origins (for flexibility)
+        origin: "*", // Allow all origins (for flexibility)
         methods: ["GET", "POST"]
     }
 });
@@ -142,5 +142,5 @@ console.log(random);
 
 // Start the server
 server.listen(3001, () => {
-    console.log("✅ Server running on http://localhost:3001");
+    console.log("✅ Server running");
 });
