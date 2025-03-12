33caef53f7f271840b01a6452d10a3bc4c3a6de8
Marc Giestin Louis Cordova <154757927+kazuha001@users.noreply.github.com>
2025-03-12 14:15:10 +0800

    Add files via upload

---
+++ Frontend/app/MainGame/GameDemoV1/src/components/hotspot.js
@@ -0,0 +1,20 @@
+const http = require("http");
+const WebSocket = require("ws");
+
+const server = http.createServer(); // Create an HTTP server
+const wss = new WebSocket.Server({ server });
+
+wss.on("connection", (ws) => {
+    console.log("Client connected");
+
+    ws.on("message", (message) => {
+        console.log("Received:", message);
+        ws.send("Hello from server!");
+    });
+
+    ws.on("close", () => console.log("Client disconnected"));
+});
+
+server.listen(8080, "192.168.55.1", () => {
+    console.log("Server running on ws://192.168.55.1:8080");
+});
