fb1452736cd4883773606c4f6ddcbfe41d5b5f40
Marc Giestin Louis Cordova <154757927+kazuha001@users.noreply.github.com>
2025-03-17 08:44:36 +0800

    Update server.ts
    Ready For Game Test

--- Backend/server.ts
+++ Backend/server.ts
@@ -112,7 +112,7 @@ instrument(io, {
 io.on("connection", (socket) => {
   console.log("A user connected! " + socket.id);
   
-  const players_length = 1 //Change this For Testing <+++++++++++++++++++++++++++++++++++++++++++++++++++ 
+  const players_length = 6 //Change this For Testing <+++++++++++++++++++++++++++++++++++++++++++++++++++ 
 
   function cleanAndListRooms() {
     const roomList = [];
